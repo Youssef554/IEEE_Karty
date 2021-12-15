50a0d185119d7dc0004a630680e351dbbd7c3831
maxmaster <youssefalkashef000@gmail.com>
2021-12-16 00:06:46 +0200

    added a new readings activity

---
+++ app/src/main/java/com/example/karty/presentation/readingsScreen/ReadingsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.karty.presentation.readingsScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.karty.R
+
+class ReadingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_readings)
+    }
+}
