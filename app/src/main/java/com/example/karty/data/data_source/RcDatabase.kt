f2161e7911c457420e43ad6fe370dd4d3894d990
maxmaster <youssefalkashef000@gmail.com>
2021-12-15 21:11:46 +0200

    added the RcResponse to the database

--- app/src/main/java/com/example/karty/data/data_source/RcDatabase.kt
+++ app/src/main/java/com/example/karty/data/data_source/RcDatabase.kt
@@ -3,8 +3,9 @@ package com.example.karty.data.data_source
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.karty.domain.model.RC
+import com.example.karty.domain.model.RcResponse
 
-@Database(entities = [RC::class], version = 1 )
+@Database(entities = [RC::class, RcResponse::class], version = 1 )
 abstract class RcDatabase:RoomDatabase() {
     abstract fun RcDao():RcDao
 
