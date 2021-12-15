6c7c52d9e96677762d0b8524c226f14c85ae63e8
maxmaster <youssefalkashef000@gmail.com>
2021-12-15 17:16:46 +0200

    added database implementation and fixed the RcResponse.kt file

--- app/src/main/java/com/example/karty/core/di/AppModule.kt
+++ app/src/main/java/com/example/karty/core/di/AppModule.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.karty.data.data_source.RcDatabase
 import com.example.karty.domain.use_cases.BluetoothConnectUseCase
 import com.example.karty.domain.use_cases.BluetoothDisconnectUseCase
 import com.example.karty.domain.use_cases.BluetoothSendCommandUseCase
@@ -12,19 +15,31 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(context: Application) =
+        Room.databaseBuilder(context, RoomDatabase::class.java, "rc_database")
+            .fallbackToDestructiveMigration().build()
+
+    @Provides
+    fun provideDao(database: RcDatabase) = database.RcDao()
+
     @Provides
     fun provideBluetoothManger(context: Application) =
         context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
 
     @Provides
-    fun provideBluetoothAdapter(bluetoothManager: BluetoothManager): BluetoothAdapter = bluetoothManager.adapter
+    fun provideBluetoothAdapter(bluetoothManager: BluetoothManager): BluetoothAdapter =
+        bluetoothManager.adapter
 
     @Provides
-    fun provideBluetoothUseCases(bluetoothAdapter: BluetoothAdapter)  = BluetoothUseCases(
+    fun provideBluetoothUseCases(bluetoothAdapter: BluetoothAdapter) = BluetoothUseCases(
         connect = BluetoothConnectUseCase(bluetoothAdapter),
         disconnect = BluetoothDisconnectUseCase(),
         sendCommand = BluetoothSendCommandUseCase(),
--- app/src/main/java/com/example/karty/domain/model/RcResponse.kt
+++ app/src/main/java/com/example/karty/domain/model/RcResponse.kt
@@ -1,8 +1,5 @@
 package com.example.karty.domain.model
 
-import androidx.room.Entity
-
-@Entity(tableName = "rc_readings")
 data class RcResponse(
     val motorRight:Int,
     val motorLeft: Int
