7fe77679d37d8634db7f6e5c65defe268a1a3e8c
maxmaster <youssefalkashef000@gmail.com>
2021-12-16 22:57:29 +0200

    saving files now works fine

--- app/src/main/java/com/example/karty/presentation/readingsScreen/ReadingsActivity.kt
+++ app/src/main/java/com/example/karty/presentation/readingsScreen/ReadingsActivity.kt
@@ -1,21 +1,32 @@
 package com.example.karty.presentation.readingsScreen
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.karty.R
 import com.example.karty.presentation.utils.ReadingsAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
+
+    const val STORAGE_PERMISSION_REQUEST_CODE = 1
 
 @AndroidEntryPoint
 class ReadingsActivity : AppCompatActivity() {
-    private lateinit var adapter:ReadingsAdapter
-    private lateinit var viewModel:ReadingsViewModel
-
+    private lateinit var adapter: ReadingsAdapter
+    private lateinit var viewModel: ReadingsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_readings)
@@ -28,15 +39,14 @@ class ReadingsActivity : AppCompatActivity() {
         if (deviceAddress != null) {
             viewModel.getReadings(deviceAddress)
         }
-        val rv:RecyclerView = findViewById(R.id.rv_ReadingsList)
+        val rv: RecyclerView = findViewById(R.id.rv_ReadingsList)
         setupRecyclerView(rv)
 
-        viewModel.devices.observe(this){
+        viewModel.readings.observe(this) {
             adapter.submitList(it)
         }
 
     }
-
 
 
     private fun setupRecyclerView(rv: RecyclerView) {
@@ -47,10 +57,64 @@ class ReadingsActivity : AppCompatActivity() {
     }
 
 
-
     override fun onSupportNavigateUp(): Boolean {
         finish()
         return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_readings_screen, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.btn_ShareDatabae -> {
+                exportData()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+private fun exportData(){
+    if (havePermissions()) {
+        val f = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+            "readings.txt"
+        )
+        viewModel.exportReadings(f)
+    }else{
+        requestStoragePermission()
+    }
+
+}
+
+    private fun havePermissions() = ActivityCompat.checkSelfPermission(
+        this,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestStoragePermission(){
+        val permissions = arrayOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        ActivityCompat.requestPermissions(this, permissions, 1)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == STORAGE_PERMISSION_REQUEST_CODE){
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+            exportData()
+        }else{
+            Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show()
+
+        }
     }
 
 }
--- app/src/main/java/com/example/karty/presentation/readingsScreen/ReadingsViewModel.kt
+++ app/src/main/java/com/example/karty/presentation/readingsScreen/ReadingsViewModel.kt
@@ -1,31 +1,52 @@
 package com.example.karty.presentation.readingsScreen
 
-import android.util.Log
+import android.app.Application
+import android.os.Environment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.karty.data.data_source.RcDao
-import com.example.karty.domain.model.RC
 import com.example.karty.domain.model.RcResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-private lateinit var DEVICE_MAC:String
+import android.content.Context.MODE_PRIVATE
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStreamWriter
+
+
+private lateinit var DEVICE_MAC: String
 
 @HiltViewModel
-class ReadingsViewModel @Inject constructor(private val dao: RcDao) : ViewModel() {
+class ReadingsViewModel @Inject constructor(
+    private val dao: RcDao,
+    private val context: Application
+) : ViewModel() {
     private val _readings: MutableLiveData<List<RcResponse>> = MutableLiveData(emptyList())
-    val devices: LiveData<List<RcResponse>> = _readings
+    val readings: LiveData<List<RcResponse>> = _readings
 
 
-
-    fun getReadings(deviceAddress:String) {
+    fun getReadings(deviceAddress: String) {
         DEVICE_MAC = deviceAddress
         viewModelScope.launch {
             _readings.value = dao.getDeviceReadings(DEVICE_MAC)
-            Log.e("ttt", "getReadings: ${_readings.value}", )
         }
     }
+
+    fun exportReadings(file: File) {
+        try {
+            file.writeText("")
+            for (reading in readings.value!!) {
+                file.appendText("${reading.dateTime} --- leftMotor: ${reading.motorLeft} --- rightMotor: ${reading.motorRight}\n")
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+
 }
